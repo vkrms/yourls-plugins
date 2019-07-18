dd7324b16692c35822865824c042e8ed4faee233
vkrms <april504@yandex.ru>
2019-07-18 11:50:29 +1000

    sprites as template

--- user/plugins/utmc/templates/utmc-bulk-template.php
+++ user/plugins/utmc/templates/utmc-bulk-template.php
@@ -8,61 +8,75 @@
 <link rel="stylesheet" href="https://unpkg.com/vue-select@latest/dist/vue-select.css">
 
 <!-- utmc stuff -->
-<link rel="stylesheet" href="<?php echo utmc_resource( 'dist/utmc.css' ) ?>"/>
+<link rel="stylesheet" href="<?php echo utmc_resource( 'src/css/utmc.css' ) ?>"/>
 
-<!-- SVG SPRITES -->
-<svg id="svg-sprites">
-	<symbol id="spr_cross" viewBox="0 0 16 16">
-		<path d="M9.2,8l4.6-4.6c0.3-0.3,0.3-0.9,0-1.2s-0.9-0.3-1.2,0L8,6.8L3.4,2.2c-0.3-0.3-0.9-0.3-1.2,0s-0.3,0.9,0,1.2L6.8,8l-4.6,4.6
-			c-0.3,0.3-0.3,0.9,0,1.2C2.4,13.9,2.6,14,2.8,14s0.4-0.1,0.6-0.2L8,9.2l4.6,4.6c0.2,0.2,0.4,0.2,0.6,0.2s0.4-0.1,0.6-0.2
-			c0.3-0.3,0.3-0.9,0-1.2L9.2,8z"/>
-	</symbol>
-
-	<symbol id="spr_caret" viewBox="0 0 16 16">
-		<path d="M8,12.6c-0.2,0-0.4-0.1-0.6-0.2L2.1,7c-0.3-0.3-0.3-0.9,0-1.2s0.9-0.3,1.2,0L8,10.5l4.7-4.7c0.3-0.3,0.9-0.3,1.2,0
-			s0.3,0.9,0,1.2l-5.3,5.3C8.5,12.5,8.3,12.6,8,12.6L8,12.6z"/>
-	</symbol>
-</svg>
+<!-- SPRITES -->
+<?php require( 'sprites.svg.php' ) ?>
 
 <!-- MARKUP -->
 <section class="utmc">
 	<form class="utmc__form" action="/" method="post">
-		<h4 class="utmc__title">Utm constructor</h4>
+		<h4 class="utmc__title">Utm Bulk Constructor</h4>
 
 		<div id="bulkApp">
 
 			<div class="utmc__flex-wrap">
 
-				<input type="text" name="utmc_base" class="utmc__text-field utmc__result" v-model="baseUrl" placeholder="base url">
+				<input type="text" name="utmc_base" class="utmc__text-field utmc_base" v-model="baseUrl" placeholder="base url">
 
 				<div class="utmc__input-group">
+					<label class="utmc_label">utm_source</label>
 					<input type="radio" name="utmc_multi" class="utmc__radio" v-model="radioModel" value="utm_source" tabindex="-1" title="click here for multiple input "/>
 					<textarea v-if="radioModel == 'utm_source'" name="utm_source" class="utmc__text-field --textarea" placeholder="utm_source, separated by newline" v-model="inputs.utm_source"></textarea>
-					<v-select v-else taggable :options="sources" name="utm_source" class="utmc__text-field" placeholder="utm_source" v-model="inputs.utm_source"></v-select>
+					<v-select v-else taggable :options="options.utm_source"
+						name="utm_source" select-on-tab :clearable="false"
+						class="utmc__text-field" placeholder="utm_source"
+						v-model="inputs.utm_source">
+					</v-select>
 				</div>
 
 				<div class="utmc__input-group">
+					<label class="utmc_label">utm_media</label>
 					<input type="radio" name="utmc_multi" class="utmc__radio" v-model="radioModel" value="utm_media" tabindex="-1" title="click here for multiple input "/>
 					<textarea v-if="radioModel == 'utm_media'" name="utm_media" class="utmc__text-field --textarea" placeholder="utm_media, separated by newline" v-model="inputs.utm_media"></textarea>
-					<v-select v-else taggable :options="media"  name="utm_media" class="utmc__text-field" placeholder="utm_media" v-model="inputs.utm_media"></v-select>
+					<v-select v-else taggable :options="options.utm_media"
+						name="utm_media" class="utmc__text-field"
+						placeholder="utm_media" select-on-tab :clearable="false"
+						v-model="inputs.utm_media">
+					</v-select>
 				</div>
 
 				<div class="utmc__input-group">
+					<label class="utmc_label">utm_campaign</label>
 					<input type="radio" name="utmc_multi" class="utmc__radio" v-model="radioModel" value="utm_campaign" tabindex="-1" title="click here for multiple input "/>
 					<textarea v-if="radioModel == 'utm_campaign'" name="utm_text-field" class="utmc__text-field --textarea" placeholder="utm_campaign, separated by newline" v-model="inputs.utm_campaign"></textarea>
-					<input v-else type="text" name="utm_campaign" class="utmc__text-field" placeholder="utm_campaign" v-model="inputs.utm_campaign">
+					<v-select v-else taggable :options="options.utm_campaign" label="value"
+						name="utm_campaign" class="utmc__text-field"
+						placeholder="utm_campaign" v-model="inputs.utm_campaign"
+						select-on-tab :clearable="false">
+					</v-select>
 				</div>
 
 				<div class="utmc__input-group">
+					<label class="utmc_label">utm_content</label>
 					<input type="radio" name="utmc_multi" class="utmc__radio" v-model="radioModel" value="utm_content" tabindex="-1" title="click here for multiple input "/>
 					<textarea v-if="radioModel == 'utm_content'" name="utm_content" class="utmc__text-field --textarea" placeholder="utm_content, separated by newline" v-model="inputs.utm_content"></textarea>
-					<input v-else type="text" name="utm_content" class="utmc__text-field" placeholder="utm_content" v-model="inputs.utm_content">
+					<v-select v-else taggable :options="options.utm_content" label="value"
+						name="utm_content" class="utmc__text-field"
+						placeholder="utm_content" v-model="inputs.utm_content"
+						select-on-tab :clearable="false">
+					</v-select>
 				</div>
 
 				<div class="utmc__input-group">
+					<label class="utmc_label">utm_term</label>
 					<input type="radio" name="utmc_multi" class="utmc__radio" v-model="radioModel" value="utm_term" tabindex="-1" title="click here for multiple input "/>
 					<textarea v-if="radioModel == 'utm_term'" name="utm_term" class="utmc__text-field --textarea" placeholder="utm_term, separated by newline" v-model="inputs.utm_term"></textarea>
-					<input v-else type="text" name="utm_term" class="utmc__text-field" placeholder="utm_term" v-model="inputs.utm_term">
+					<v-select v-else taggable :options="options.utm_term" label="value"
+						name="utm_term" class="utmc__text-field" select-on-tab
+						placeholder="utm_term" v-model="inputs.utm_term"
+						select-on-tab :clearable="false">
+					</v-select>
 				</div>
 
 				<!-- <textarea name="utmc_urls" class="utmc__text-field utmc__result" v-bind:value="urls" placeholder="result here..."></textarea> -->
--- user/plugins/utmc/templates/utmc-template.php
+++ user/plugins/utmc/templates/utmc-template.php
@@ -7,26 +7,11 @@
 <script src="https://unpkg.com/vue-select@latest"></script>
 <link rel="stylesheet" href="https://unpkg.com/vue-select@latest/dist/vue-select.css">
 
-<!-- axios -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script>
-<!-- <script src="https://unpkg.com/axios@latest"></script> -->
-
 <!-- utmc stuff -->
-<link rel="stylesheet" href="<?php echo utmc_resource( 'dist/utmc.css' ) ?>"/>
+<link rel="stylesheet" href="<?php echo utmc_resource( 'src/css/utmc.css' ) ?>"/>
 
-<!-- SVG SPRITES -->
-<svg id="svg-sprites">
-	<symbol id="spr_cross" viewBox="0 0 16 16">
-		<path d="M9.2,8l4.6-4.6c0.3-0.3,0.3-0.9,0-1.2s-0.9-0.3-1.2,0L8,6.8L3.4,2.2c-0.3-0.3-0.9-0.3-1.2,0s-0.3,0.9,0,1.2L6.8,8l-4.6,4.6
-			c-0.3,0.3-0.3,0.9,0,1.2C2.4,13.9,2.6,14,2.8,14s0.4-0.1,0.6-0.2L8,9.2l4.6,4.6c0.2,0.2,0.4,0.2,0.6,0.2s0.4-0.1,0.6-0.2
-			c0.3-0.3,0.3-0.9,0-1.2L9.2,8z"/>
-	</symbol>
-
-	<symbol id="spr_caret" viewBox="0 0 16 16">
-		<path d="M8,12.6c-0.2,0-0.4-0.1-0.6-0.2L2.1,7c-0.3-0.3-0.3-0.9,0-1.2s0.9-0.3,1.2,0L8,10.5l4.7-4.7c0.3-0.3,0.9-0.3,1.2,0
-			s0.3,0.9,0,1.2l-5.3,5.3C8.5,12.5,8.3,12.6,8,12.6L8,12.6z"/>
-	</symbol>
-</svg>
+<!-- SPRITES -->
+<?php require( 'sprites.svg.php' ) ?>
 
 <!-- MARKUP -->
 <section class="utmc">
